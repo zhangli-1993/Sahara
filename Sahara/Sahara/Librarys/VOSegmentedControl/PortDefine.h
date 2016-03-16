81c7ddfaeac1e453b69dc1a0b2a0ef3db2700b35
YanWeiMin0523 <1379556026@qq.com>
2016-03-16 10:48:30 +0800

    请求

---
+++ Sahara/Sahara/Librarys/VOSegmentedControl/PortDefine.h
@@ -0,0 +1,20 @@
+//
+//  PortDefine.h
+//  Sahara
+//
+//  Created by scjy on 16/3/16.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#ifndef PortDefine_h
+#define PortDefine_h
+
+//首页segement
+#define kSegementort @"http://mrobot.pcauto.com.cn/configs/pcauto_v4_cms_android_channel_tree"
+
+
+
+
+
+
+#endif /* PortDefine_h */
