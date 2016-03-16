c304f2f9375e2e1fe4b7c47437fab7b545b8e70d
张莉 <287081764@qq.com>
2016-03-16 08:10:24 +0800

    建工程

---
+++ Sahara/Sahara/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sahara
+//
+//  Created by scjy on 16/3/15.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
