3c65e368e8dff7c46cc6f7e55fa79bfb756db9a9
乔自朋 <1320280834@qq.com>
2016-03-17 20:27:16 +0800

    qiao

---
+++ Sahara/Sahara/Calsses/Forum/View/ForumOneTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  ForumOneTableViewCell.h
+//  Sahara
+//
+//  Created by scjy on 16/3/17.
+//  Copyright © 2016年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ForumModel.h"
+@interface ForumOneTableViewCell : UITableViewCell
+@property (nonatomic, strong)ForumModel *forumModel;
+@end
